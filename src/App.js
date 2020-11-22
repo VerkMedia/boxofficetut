c388ca74802a7d1fdab973e1052312b3e0ed12a2
verkmedia <josephvarecha@gmail.com>
2020-11-22 11:52:40 -0500

    created page components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs'
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    
+    <div>
+      <Navs />
+
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>404</Route>
+    </Switch>
+    
     </div>
   );
 }
 
 export default App;
+
+
